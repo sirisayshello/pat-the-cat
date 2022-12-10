0a21edc0541188d0d9768980842daaaf03e8e705
sirisayshello <veckovis.salongen0p@icloud.com>
2022-12-10 16:31:40 +0100

    Scroll event listener

---
+++ scripts/scroll.js
@@ -0,0 +1,8 @@
+const spinObject = () => {
+  const catImages = document.querySelectorAll(".cat-container img");
+  catImages.forEach((catImg) => {
+    catImg.classList.add("spin");
+  });
+};
+
+window.addEventListener("scroll", spinObject);
