af0549080fea5569a107eaeff3c3583a9aeed0f3
sirisayshello <veckovis.salongen0p@icloud.com>
2022-12-10 20:05:10 +0100

    made prettier happy

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,8 +1,8 @@
 const spinObject = () => {
-  const catImages = document.querySelectorAll(".cat-container img");
+  const catImages = document.querySelectorAll('.cat-container img');
   catImages.forEach((catImg) => {
-    catImg.classList.add("spin");
+    catImg.classList.add('spin');
   });
 };
 
-window.addEventListener("scroll", spinObject);
+window.addEventListener('scroll', spinObject);
